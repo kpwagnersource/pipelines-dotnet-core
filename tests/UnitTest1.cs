e357ec80d1172ec41b096d3c427c62220d96dca9
Kyle Wagner <kwagner@sourceallies.com>
2020-06-10 17:02:54 -0500

    Change file structure and add tests dir

---
+++ tests/UnitTest1.cs
@@ -0,0 +1,25 @@
+using System;
+using Xunit;
+
+namespace tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(4,Add(2,2));
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            Assert.Equal(4, Add(1,3));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
